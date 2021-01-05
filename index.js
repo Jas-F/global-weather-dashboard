57e3e15896b7be5fb632cde1c88833cfbdd8cccd
Jas-F <jasminer.franklin@gmail.com>
2021-01-05 00:26:17 -0500

    add skeleton

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-/Filter list
+document(ready)
 
-$(document).ready(function(){
-  $("#listSearch").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myList li").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
+// on start preform 3 functions
+// 1. add city to search history 
+    // 1.1 append city to search history box
+    // 1.1a create empty array & push search name into array
+// 2. display current city weather in top upper card
+    // 2.1 get city name
+    // 2.2 get temperature
+    // 2.3 get humidity
+    // 2.4 get wind speed
+    // 2.5 get UV index
+      // 2.5a value if UV index 
+      // 2.5b color coding accord to value
+    // 2.6 get weather icon
+// 3. display 5 day forecast in bottom 5 cards
+    // 
+// create if function, if uv index is between certain rows make background red, yellow, green.
+// be able to click on search history and display time
+var API key=f68b613983e0ae131f5d600baa16f997
+var city= userinput
