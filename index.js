54824da772d35da50c632b6cf11c5353cca1b7db
Jas-F <jasminer.franklin@gmail.com>
2021-03-16 14:19:30 -0400

    create for loop with onclick

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // create empty array for search history
-var history = []
-var cities
-// create onclick that grabs value of search form
-$('#search').on('click', function(event){
-event.preventDefault()
-var searchValue= $('#citySearch').val()
-console.log(searchValue)
-// insert search value into current weather and five day function
-// so it completes the function with the parameter of the search value
-// so current weather of the searched city can be found 
-currentWeather(searchValue)
-fiveDay(searchValue)
-// conditional statement if search history does not have the searched city in the array or in the history array then push into array
-// tracking searched cities inside of the app
-if(!history.includes(searchValue))
-history.push(searchValue)
-console.log(history)
-// uvIndex(searchValue)
-// turn history array inside of local storage into a string
-localStorage.setItem("cities",JSON.stringify(history))
+  var history = []
+  var cities
+  // create onclick that grabs value of search form
+  $('#search').on('click', function (event) {
+    event.preventDefault()
+    var searchValue = $('#citySearch').val()
+    console.log(searchValue)
+    // insert search value into current weather and five day function
+    // so it completes the function with the parameter of the search value
+    // so current weather of the searched city can be found 
+    currentWeather(searchValue)
+    fiveDay(searchValue)
+    // conditional statement if search history does not have the searched city in the array or in the history array then push into array
+    // tracking searched cities inside of the app
+    if (!history.includes(searchValue))
+      history.push(searchValue)
+    console.log(history)
+    // uvIndex(searchValue)
+    // turn history array inside of local storage into a string
+    localStorage.setItem("cities", JSON.stringify(history))
 
-// convert local storage data to javascript object
-cities= JSON.parse((localStorage.getItem("cities")))
-// clear the history
-$('.history').empty()
-// iterate through cities from local storage and append to to webpage
-for (i=0; i<cities.length; i++){
-  $(".history").append(cities[i])
-}
+    // convert local storage data to javascript object
+    cities = JSON.parse((localStorage.getItem("cities")))
+    // clear the history
+    $('.history').empty()
+    // iterate through cities from local storage and append to to webpage
+    for (i = 0; i < cities.length; i++) {
+      var listItem = $('<li>').text(cities[i]).on("click", function (event) {
+        var searchValue = cities[i]
+        currentWeather(searchValue)
+        fiveDay(searchValue)
+      })
+      $(".history").append(listItem)
+    }
+   
+  })
 });
 
-var APIkey="f68b613983e0ae131f5d600baa16f997"
+var APIkey = "f68b613983e0ae131f5d600baa16f997"
 
-function currentWeather(searchValue){
+function currentWeather(searchValue) {
   // ajax call to get current weather when given a search value a.k.a searched city & Api key
   $.ajax({
     type: "GET",
@@ -49,15 +56,15 @@ function currentWeather(searchValue){
       $('.card-hum').text(`Humidity: ${response.main.humidity}`)
       $('.card-wind').text(`Wind Speed: ${response.wind.speed}`)
       // get cities longitude and latitude and pass it into uvIndex function
-      var lat= response.coord.lat
-      var lon= response.coord.lon
-      uvIndex(lat,lon)
+      var lat = response.coord.lat
+      var lon = response.coord.lon
+      uvIndex(lat, lon)
     }
   });
 };
 
 
-function fiveDay(searchValue){
+function fiveDay(searchValue) {
   // ajax call for weather forecast when given search value and api key 
   $.ajax({
     type: "GET",
@@ -67,28 +74,29 @@ function fiveDay(searchValue){
       console.log(response)
       $('.forecast').empty()
       // iterate through list of daily and hourly forecast 
-      for (i=0; i<response.list.length; i++){
+      for (i = 0; i < response.list.length; i++) {
         // conditional statement if list includes 6 p.m. meaning weather at 6 p.m. each day complete this function
-        if(response.list[i].dt_txt.indexOf("18:00:00")!== -1){
+        if (response.list[i].dt_txt.indexOf("18:00:00") !== -1) {
           // build 5 day forecast card and body using jquery
           var col = $('<div>').addClass('col-2')
-          var card= $('<div>').addClass('card')
-          var body= $('<div>').addClass('card-body')
+          var card = $('<div>').addClass('card')
+          var body = $('<div>').addClass('card-body')
           // create date object using constructor 
           // get date data from api response
           // convert date object to string
           var date = $('<h6>').text(new Date(response.list[i].dt_txt).toLocaleDateString())
           // create key of temp to equal value temp from list of forecast data
           // create key of icon to equal icon from list of forecast data
-          var temp=  $('<p>').text(`Temp: ${response.list[i].main.temp}`)
+          var icon = $()
+          var temp = $('<p>').text(`Temp: ${response.list[i].main.temp}`)
           // create key of humidity to equal value from list array humidity
-          var humid= $('<p>').text(`Humidity: ${response.list[i].main.humidity}`)
+          var humid = $('<p>').text(`Humidity: ${response.list[i].main.humidity}`)
 
           // put all of the variables together to construct forcast cards when city is searched/function is called
           // in a colume of 2 append a card then append a card body to each card, append api response from list array to card body
           // append icon to card body
-        col.append(card.append(body.append(date,temp,humid)))
-        $('.forecast').append(col)
+          col.append(card.append(body.append(date, temp, humid)))
+          $('.forecast').append(col)
         }
       }
       // $(".card").text(response.list[2]);
@@ -103,11 +111,11 @@ function uvIndex(lat, lon) {
     url: `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIkey}`,
     dataType: "json",
     success: function (response) {
-      console.log(response) 
-      $('.card-uv').empty() 
+      console.log(response)
+      $('.card-uv').empty()
       // inside card uv div create p tag return text content of UV Index key with a value of response value
       var value = $('<p>').text(`UV Index: ${response.value}`)
-      $(`.card-uv`).append(value) 
+      $(`.card-uv`).append(value)
     }
   });
 }
@@ -127,4 +135,3 @@ console.log(history)
 // display whether icon
 // get icon forecast api response
 
-});
